05f1b855fbb6823f892fa353ae994c6b5bfe5a4e
Cloudchirp <cloudchirp.chirpmaster@gmail.com>
2013-02-27 16:32:07 -0500

    Added pg to Gemfile; added production db config.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #puts "ENV['TWITTER_KEY'] = #{ENV['TWITTER_KEY']}"
-  #puts "ENV['TWITTER_SECRET'] = #{ENV['TWITTER_SECRET']}"
-  
+
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   #provider :jira, ENV['JIRA_KEY'], ENV['JIRA_SECRET']
 end
